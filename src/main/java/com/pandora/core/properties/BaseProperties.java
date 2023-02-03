0600fe602e640e14eac3634ecc196336998d7b93
wing9537 <tommywing37@gmail.com>
2023-02-03 10:15:57 +0000

    create api doc with swagger ui

--- src/main/java/com/pandora/core/properties/BaseProperties.java
+++ src/main/java/com/pandora/core/properties/BaseProperties.java
@@ -12,4 +12,6 @@ public class BaseProperties {
 
     private String appName;
 
+    private String version;
+
 }
---
+++ src/main/java/com/pandora/jpx/config/SpringDocConfig.java
@@ -0,0 +1,33 @@
+package com.pandora.jpx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "JPX API", version = "${base.version}")
+)
+public class SpringDocConfig {
+    
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement()
+            .addList(securitySchemeName))
+            .components(new Components()
+            .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+            .name(securitySchemeName)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")));
+        }
+
+}
